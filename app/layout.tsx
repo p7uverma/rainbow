93bf8b2625e485fbef0eedeeb28970ccbdf24790
Priyanshu Verma <priyanshu.creator.dev@gmail.com>
2023-10-12 12:35:32 +0530

    default dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,7 +81,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Toaster />
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
           <Navbar />
           <div className="flex flex-col min-h-screen">
             <div className="flex-grow pb-10 pt-24">{children}</div>
